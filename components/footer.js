d25de161562a469a42df324784ec0b85fce83a4e
Akash Patil <akashpatil5118@gmail.com>
2022-03-31 17:05:35 +0530

    done
    done

---
+++ components/footer.js
@@ -0,0 +1,96 @@
+const footer = () => ` <div class="knowus" style="color: white;">
+<div id="know">
+    <h3 id="porter">PORTER</h3>
+    <ul class="porter">
+        <li>Send anything</li>
+        <li>anywhere</li>
+        <li>anytime</li>
+    </ul>
+</div>
+<div id="know2">
+    <h3>Company</h3>
+    <ul>
+        <li><a href="">About us</a></li>
+        <li><a href="">offers</a></li>
+        <li><a href="">Careers</a></li>
+        <li><a href="">Blog</a></li>
+    </ul>
+</div>
+<div id="know3">
+    <h3>Services</h3>
+    <ul>
+        <li><a href="">City Tempo</a></li>
+        <li><a href="">For Enterprise</a></li>
+        <li><a href="">Packers & Movers</a></li>
+    </ul>
+</div>
+<div id="know3">
+    <h3>Support</h3>
+    <ul>
+        <li><a href="">Chat with us <i class="fab fa-whatsapp"></i></a></li>
+        <li><a href="">Need Help?</a></li>
+        <li><a href="">Terms of Service</a></li>
+        <li><a href="">Insurance FAQs</a></li>
+    </ul>
+</div>
+<div id="know4">
+    <h3>Download App</h3>
+    <div id="dowapp">
+        <img id="img-1"
+            src="https://www.freepnglogos.com/uploads/play-store-logo-png/play-store-logo-nisi-filters-australia-11.png">
+        <img id="img-2" src="https://www.vortex6.com/wp-content/uploads/2019/09/app-store.png">
+    </div>
+</div>
+</div>
+<div class="knowus" style="color: white;">
+<div id="know">
+    <h3>We are here</h3>
+    <ul>
+        <li><a href="">Mumbai</a></li>
+        <li><a href="">Ahmedabad</a></li>
+        <li><a href="">Lucknow</a></li>
+    </ul>
+</div>
+<div id="know2">
+    <ul>
+        <li><a href="">Delhi NCR</a></li>
+        <li><a href="">Jaipur</a></li>
+        <li><a href="">Coimbatore</a></li>
+    </ul>
+</div>
+<div id="know3">
+    <ul>
+        <li><a href="">Bangalore</a></li>
+        <li><a href="">Pune</a></li>
+        <li><a href="">Indore</a></li>
+    </ul>
+</div>
+<div id="know3">
+    <ul>
+        <li><a href="">Hyderabad</a></li>
+        <li><a href="">Kolkata</a></li>
+        <li><a href="">Nagpur</a></li>
+    </ul>
+</div>
+<div id="know3">
+    <ul>
+        <li><a href="">Chennai</a></li>
+        <li><a href="">Surat</a></li>
+        <li><a href="">Chandihgarh</a></li>
+    </ul>
+</div>
+</div>
+<div class="last-foot">
+<div id="sociallink">
+    <i class="fab fa-facebook"></i>
+    <i class="fab fa-twitter"></i>
+    <i class="fab fa-youtube"></i>
+    <i class="fab fa-instagram"></i>
+</div>
+<div class="text">
+    <p>2021 SmartShift Logistics Solutions Pvt. Ltd.
+        CIN: U74999MH2014PTC306120</p>
+</div>
+</div>`
+
+export default footer
